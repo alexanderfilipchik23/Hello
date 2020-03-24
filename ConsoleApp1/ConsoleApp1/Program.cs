54569a43321443e4a7ae5419722383489f771e1d
Krumis <rpr_2021@list.ru>
2020-03-24 08:28:33 +0300

    Hello

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] colishestvo)
+        {
+            Tovar Rushka = new Tovar();               // вызов 1-ого конструктора без параметров
+            Tovar Mesh = new Tovar("Мяч");            //вызов 2-ого конструктора с одним параметром
+            Tovar sterka = new Tovar("Стёрка", 25,3); // вызов 3-его конструктора с тремя параметрами
+
+            Mesh.GetInfo();
+            Rushka.GetInfo();
+            sterka.GetInfo();
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+   
+}
+
+
+    
+
---
+++ ConsoleApp1/ConsoleApp1/Tovar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Tovar
+    {
+        private string naminovanie;
+        private int colishestvo = 0;
+        private float stoimosti = 0;
+
+       public  Tovar() { naminovanie = "Неизвестно"; colishestvo = 18; stoimosti = 12;}      // 1 конструктор
+
+        public Tovar(string n) { naminovanie = n; colishestvo = 18; stoimosti = 12;}         // 2 конструктор
+
+        public Tovar(string n, int a, float v) { naminovanie = n; colishestvo = a; stoimosti = v;}   // 3 конструктор
+
+        public void GetInfo()
+
+        {
+            Console.WriteLine($":Наименование {naminovanie}  Количество: {colishestvo} Стоймость: {stoimosti} ");
+        }
+
+    }
+}
+
+
+
+
+
+
